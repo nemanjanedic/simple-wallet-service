1a1d6b9f37993b87c1678cee3d488144b76f6304
Nemanja Nedic <nemanjanedic@gmail.com>
2026-06-28 14:57:07 +0200

    Initial Spring Boot project setup.

---
+++ src/test/java/com/nemanjanedic/simplewallet/SimpleWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemanjanedic.simplewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
